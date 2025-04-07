8855ddb0dea98f9bf70d6ce0b81ba86f020934a7
ANAS MUFTI <muftianas97@gmail.com>
2025-04-07 17:57:52 +0500

    list and wordso  pyhone

--- main.py
+++ main.py
@@ -1,8 +1,104 @@
-print("hello World")
+# print("hello World")
+# a=5
+# b=20
+# #dcscscsd  comments
 
-#dcscscsd  comments
+# anas ='''multi line String Data'''
 
-anas ='''multi line String Data '''
+# print("hello "+anas)
+# print(anas[-3:-1])
 
-print("hello "+anas)
-print(anas[-3:-1])
+
+# if (a<b):
+#     print("True")
+# else:
+#     print("False")
+
+
+# if(a>b):
+#     print(a+"is Greater")
+# elif(a==b):
+#     print("Equal")
+# elif(a!=b):
+#     print("Not Equal")
+# else:
+#     print("False")  
+
+
+# import datetime
+
+# now = datetime.datetime.now()
+
+# print("Current Time is:", now.strftime("%H:%M:%S"))  
+
+# day = input("Enter a day: ")
+
+# # Match-case to handle different values
+# match day:
+#     case "Monday":
+#         print("Start of the week!")
+#     case "Friday":
+#         print("Weekend is near!")
+#     case "Sunday":
+#         print("Time to relax!")
+#     case _:
+#         print("It's just a regular day.")
+
+
+# name = "Anas Mufti"
+
+# for i in name:
+#     print(i)
+
+
+# i = 0
+# while (i < len(name)):
+#     print(name[i])
+#     i += 1
+
+ 
+
+#  BREAK (end the loop)
+# CONTINUE (dismiss the current ateration and move to next )
+
+
+# def check_even_odd(num):
+#     if num % 2 == 0:
+#         print(f"{num} is even")
+#     else:
+#         print(f"{num} is odd")
+
+
+# check_even_odd(10)
+
+# mark=[1,2,3,4,5,6,7,8,9,10]
+# print(mark[-3]) # 0 to 4
+
+
+
+# def greet(name):
+#     """
+#     This function greets the person whose name is passed.
+#     """
+#     print("Hello,", name)
+
+# # greet("Anas")
+# print(greet.__doc__)
+
+
+numbers = [1, 3, 5, 7, 9]
+
+# for num in numbers:
+#     if num == 4:
+#         print("Found 4!")
+#         break
+# else:
+    
+#     print("4 not found in the list.")
+
+
+# for num in numbers:
+#    print(f"Found {num}")
+        
+# else:
+#     print("4 not found in the list.")   
