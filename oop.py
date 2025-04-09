856e5488e30dfb573a758913b5fdeac8dcd3d24a
ANAS MUFTI <muftianas97@gmail.com>
2025-04-09 19:04:47 +0500

    static

--- oop.py
+++ oop.py
@@ -13,25 +13,52 @@
 # DECORATORS = Take function as an argument and modify it 
 
 
-def decorator_function(original_function):
-    def wrapper_function():
-        print("Wrapper executed before the original function")
-        original_function()
-        print("Wrapper executed after the original function")
-    return wrapper_function
+# def decorator_function(original_function):
+#     def wrapper_function():
+#         print("Wrapper executed before the original function")
+#         original_function()
+#         print("Wrapper executed after the original function")
+#     return wrapper_function
 
 
-
-@decorator_function
-def display():
-    print("Display function executed")  
-
-
-
-
+# @decorator_function
+# def display():
+#     print("Display function executed")  
 
 # decorator_function(display)()  # This will return the wrapper function/
-display()  # Output: Wrapper executed before the original function    
+# display()  # Output: Wrapper executed before the original function  
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+#   
+
+# class Person:
+#     def __init__(self, name):
+#         self._name = name  # _name is considered 'protected'
+
+#     @property
+#     def name(self):
+#         print("Getting name...")
+#         return self._name
+
+#     @name.setter
+#     def name(self, value):
+#         print("Setting name...")
+#         if not isinstance(value, str):
+#             raise ValueError("Name must be a string")
+#         self._name = value
+
+
+# p = Person("Anas")
+# print(p.name)      # Getter called
+# p.name = "Ali"     # Setter called
+# print(p.name)
 
 
 
+# NO use of self in the function if it is static method
