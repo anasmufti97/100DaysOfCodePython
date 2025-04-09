016fc8c608b193b372436715f7a06fd6bcaee7c4
ANAS MUFTI <muftianas97@gmail.com>
2025-04-09 15:16:27 +0500

    OOP

--- oop.py
+++ oop.py
@@ -7,3 +7,13 @@ class MyClass:
         print(f"Hello, {self.name}!")
 
 
+
+
+# Creating an instance of MyClass
+my_object = MyClass("Anas")
+my_object.greet()  # Output: Hello, Anas!     
+
+
+
+
+# DECORATORS = Take function as an argument and modify it 
